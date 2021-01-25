374959cab5a59576d9ff191ba1fe2a56a934586d
Alexander Juda <alexanderjuda@gmail.com>
2021-01-25 13:20:38 +0100

    Draw basic bricks

--- curs_noid.lua
+++ curs_noid.lua
@@ -1,5 +1,7 @@
 local curses = require "curses"
 
+-- curses wrappers
+
 local function make_screen()
   local screen = curses.initscr()
   -- TODO: add screen configuration
@@ -23,13 +25,52 @@ local function read_char(screen)
   return screen:getch()
 end
 
+local function move_cursor(screen, y, x)
+  screen:move(y, x)
+end
+
+-- game rendering
+
+function paddleLine(opts)
+  return string.rep(" ", opts.pos) .. string.rep(opts.char, opts.length)
+end
+
+function brickFill(length)
+  assert(2 <= length, "brick too short: " .. length)
+
+  return "[" .. string.rep("=", length - 2) .. "]"
+end
+
+function brickPlaceholder(length)
+  return string.rep(" ", length)
+end
+
+function brickLine(filledBrickIndices, brickLength)
+  line = ""
+  prevBrickI = 0
+
+  for _, brickI in ipairs(filledBrickIndices) do
+    for fillI = prevBrickI, brickI - 2 do
+      line = line .. brickPlaceholder(brickLength)
+    end
+
+    line = line .. brickFill(brickLength)
+
+    prevBrickI = brickI
+  end
+
+  return line
+end
+
 
 local function main()
   local screen = make_screen()
 
   print_text(screen, 5, 20, "hello!")
+  print_text(screen, 2, 1, brickLine({ 1, 2, 4 }, 4))
   render(screen)
 
+  move_cursor(screen, 0, 0)
   read_char(screen)
 
   cleanup_screen()
